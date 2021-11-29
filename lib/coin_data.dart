c28510a8558f8a6390f9e4661a372f1edd9b4a19
Jonas <swejoz19@htl-kaindorf.at>
2021-11-29 17:47:43 +0100

    upload

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,32 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+// const apiKey = 'F0FCC32D-6B16-46E6-B0DE-E0C9E173DDD5';
+// const apiKey = 'ACC479B5-F79F-4349-A9DC-91027351D9E8';
+const apiKey = '4B4C2ECC-C808-4072-BC5B-BB41246C3A61';
+const coinapiURL = 'https://rest.coinapi.io/v1/exchangerate';
+
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    //4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
+    //5: Return a Map of the results instead of a single value.
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      //Update the URL to use the crypto symbol from the cryptoList
+      String requestURL =
+          '$coinapiURL/$crypto/$selectedCurrency?apikey=$apiKey';
+      Uri requestURI = Uri.parse(requestURL);
+      http.Response response = await http.get(requestURI);
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double lastPrice = decodedData['rate'];
+        //Create a new key value pair, with the key being the crypto symbol and the value being the lastPrice of that crypto currency.
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
+    }
+    return cryptoPrices;
+  }
+}
